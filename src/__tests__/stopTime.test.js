4d923dbb80f4d0d1f5fed75cbe34154842f90630
Телицын Никита <dmtvn.labs@yandex.ru>
2022-06-10 20:09:23 +0300

    commit

--- src/__tests__/stopTime.test.js
+++ src/__tests__/stopTime.test.js
@@ -1,9 +1,11 @@
 import { stopTime } from "../services/stopTime";
 
-test('return completed task', () => {
-    expect(stopTime(12, {completed: true})).toMatch(/Задача завершена/)
-})
-
-test('return time', () => {
-    expect(stopTime(12, {completed: false})).toMatch(/Задача создана 12 секунд назад/)
+describe('check function wich return string about time', () => {
+    test('return completed task', () => {
+        expect(stopTime(12, {completed: true})).toMatch(/Задача завершена/)
+    })
+    
+    test('return time', () => {
+        expect(stopTime(12, {completed: false})).toMatch(/Задача создана 12 секунд назад/)
+    })
 })
