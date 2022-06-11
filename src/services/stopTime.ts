970159e8a3d4c8c07fe4c2faffb62c28b208b19c
Телицын Никита <dmtvn.labs@yandex.ru>
2022-06-11 16:21:12 +0300

    commit

--- src/services/stopTime.ts
+++ src/services/stopTime.ts
@@ -4,8 +4,16 @@ export const stopTime: TFunc = (time, completed) => {
     if (completed) {
         return `Задача завершена`
     } else if (time < 60) {
-        return `Задача создана ${time} секунд назад`
+        if (time.toString().endsWith('1')) {
+            return `Задача создана ${time} секунду назад`
+        } else {
+            return `Задача создана ${time} секунды назад`
+        }
     } else {
-        return `Задача создана ${Math.floor(time / 60)} минут`
+        if (time.toString().endsWith('1')) {
+            return `Задача создана ${Math.floor(time / 60)} минуту`
+        } else {
+            return `Задача создана ${Math.floor(time / 60)} минуты`
+        }
     }
 }
